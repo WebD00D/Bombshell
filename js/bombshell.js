caaede11acdc773d9d718e230550dbeba2cbc81f
Christian <Bryant>
2015-11-12 17:15:18 -0500

    layout of bombshell index page - desktop only

---
+++ js/bombshell.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+
+  //initilizers
+  Parse.initialize("6lwXuEam4Zl0heDrMSPQtRx2xcaLM792aRNjw1gF", "hvatZD2xQvA6mmL77SDQsc9T6kqrqLo8PbUmDswC");
+  $(".button-collapse").sideNav();
+  $('.parallax').parallax();
+
+
+
+})
