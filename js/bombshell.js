434aaa1b42a8ff91fcf0fceb7380ec687c971c5e
Christian <Bryant>
2015-11-30 09:48:31 -0500

    working on service menu

--- js/bombshell.js
+++ js/bombshell.js
@@ -5,9 +5,16 @@ $(document).ready(function(){
   Parse.initialize("6lwXuEam4Zl0heDrMSPQtRx2xcaLM792aRNjw1gF", "hvatZD2xQvA6mmL77SDQsc9T6kqrqLo8PbUmDswC");
   $(".button-collapse").sideNav();
   $('.parallax').parallax();
+  $('select').material_select();
 
 
-
+  $(".card").mouseover(function(){
+    $(this).css("cursor","pointer");
+    $(this).addClass("z-depth-5");
+  })
+  $(".card").mouseout(function(){
+    $(this).removeClass("z-depth-5");
+  })
 
   $(".storytile").mouseover(function(){
     $(this).css("cursor","pointer");
